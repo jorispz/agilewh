6038dd3f02ea2d2ecde23b2e36ecdabb1f890a2d
Joris Portegies Zwart <joris.portegies.zwart@chess-ix.com>
2014-10-06 15:48:48 +0200

    Add database script for JAWSWH

--- resources/create-database.sql
+++ resources/create-database.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS `jawswh`;
-DROP USER 'jawswh';
-CREATE DATABASE `jawswh`;
-CREATE USER 'jawswh' IDENTIFIED BY 'jawswh';
-GRANT ALL ON `jawswh`.* TO 'jawswh';
+DROP DATABASE IF EXISTS `agilewh`;
+DROP USER 'agilewh';
+CREATE DATABASE `agilewh`;
+CREATE USER 'agilewh' IDENTIFIED BY 'agilewh';
+GRANT ALL ON `agilewh`.* TO 'agilewh';
