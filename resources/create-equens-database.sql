ed4947b8fca2899876b70e3aa4f59e305b912906
Joris Portegies Zwart <joris.portegies.zwart@chess-ix.com>
2015-01-27 12:00:37 +0100

    Fixed typo

--- resources/create-equens-database.sql
+++ resources/create-equens-database.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS `equenswh`;
 DROP USER 'equenswh';
 CREATE DATABASE `equenswh`;
-CREATE USER 'equenswh' IDENTIFIED BY 'equenswhilewh';
+CREATE USER 'equenswh' IDENTIFIED BY 'equenswh';
 GRANT ALL ON `equenswh`.* TO 'equenswh';
